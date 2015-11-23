6813a086bb2e54b39c90dae9d56618cbc8169c6f
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-11-23 04:54:00 -0500

    better testing

--- testrunner.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python intellij-community/python/helpers/pycharm/utrunner.py $1
---
+++ travis/testinstaller.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget http://plugins.jetbrains.com/files/7322/17496/python-community-3.zip
+
+unzip python-community-3.zip
