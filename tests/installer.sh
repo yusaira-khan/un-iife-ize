1a1f1d7ceb41ff1beca3e58432c5f5ac80e8626f
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-11-25 00:11:08 -0500

    installer in test

---
+++ tests/installer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#git clone https://github.com/JetBrains/intellij-community
+#
+
+wget http://plugins.jetbrains.com/files/7322/17496/python-community-3.zip && unzip python-community-3.zip
---
+++ travis/utrunner.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python python-community-3/python/helpers/pycharm/utrunner.py $1
